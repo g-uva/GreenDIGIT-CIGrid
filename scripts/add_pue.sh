72954cfa800021a324f7bece911f22d4b5e114d6
Gonçalo <g.j.teixeiradepinhoferreira@uva.nl>
2025-09-03 11:01:26 +0200

    Adding output files to output folder.

--- scripts/add_pue.sh
+++ scripts/add_pue.sh
@@ -1,1 +1,3 @@
-python gocdb_postprocess/gocdb_postprocess.py site_latlng.json sites_enriched.json
+#!/bin/bash
+mkdir -p output
+python gocdb_postprocess/gocdb_postprocess.py output/site_latlng.json output/sites_enriched.json
--- scripts/gen_site_coords.sh
+++ scripts/gen_site_coords.sh
@@ -1,2 +1,2 @@
 export $(grep -v '^#' .env | xargs) # To export the environment variables (for authentication)
-python goc_db_fetch_service/fetch_goc_db.py --format json --output site_latlng.json --scope EGI
+python goc_db_fetch_service/fetch_goc_db.py --format json --output output/site_latlng.json --scope EGI
