d1d4247e5bbdcd95cbb5dca21b46f42ce5208233
Gonçalo <g.j.teixeiradepinhoferreira@uva.nl>
2025-09-03 20:28:21 +0200

    Adding requirements and configuration for CI docker container.

--- scripts/start_ci_docker.sh
+++ scripts/start_ci_docker.sh
@@ -3,7 +3,7 @@ set -e
 # Build the image (only needed if you haven’t pushed/pulled it already)
 docker build -t goncaloferreirauva/gd-ci-service -f ./ci_calc_service/Dockerfile ./ci_calc_service
 # Run the container
-docker run --rm -t -p 8011:8011 \
+docker run --rm -t -p 8011:8011 -d \
   --env AUTH_VERIFY_URL=http://127.0.0.1:8000/gd-cim-api/verify_token \
   --env-file .env \
   goncaloferreirauva/gd-ci-service \
